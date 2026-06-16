2109de18eba037fd6f4d82a1fa805a52bc7ac9ae
NimishaMalreddy <80394404+NimishaMalreddy@users.noreply.github.com>
2026-06-16 17:05:46 -0400

    Initial Commit

---
+++ food-journal/food_journalApp.swift
@@ -0,0 +1,17 @@
+//
+//  food_journalApp.swift
+//  food-journal
+//
+//  Created by Nimisha on 6/16/26.
+//
+
+import SwiftUI
+
+@main
+struct food_journalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ food-journalTests/food_journalTests.swift
@@ -0,0 +1,16 @@
+//
+//  food_journalTests.swift
+//  food-journalTests
+//
+//  Created by Nimisha on 6/16/26.
+//
+
+import Testing
+
+struct food_journalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
